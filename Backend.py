db70655a7af49048aa0a341f4f0d7843e047a37f
jordi <yeruyarden@gmail.com>
2023-04-01 00:54:04 +0300

    added a DB class

--- Backend.py
+++ Backend.py
@@ -5,11 +5,11 @@ from pymongo import MongoClient
 import argparse
 
 parser = argparse.ArgumentParser(description='Start the service on two servers.')
-parser.add_argument('primary', help='IP address of the primary server')
-parser.add_argument('secondary', help='IP address of the secondary server')
+parser.add_argument('primary', help='IP address of the primary DB')
+parser.add_argument('secondary', help='IP address of the secondary DB')
 args = parser.parse_args()
 primary = args.primary
-secondary = args.secoundery
+secondary = args.secondary
 app = FastAPI()
 
 
@@ -20,16 +20,25 @@ class Task():
         self.creation_time = datetime.now()
         self.task_id = str(uuid.uuid4())
 
+    def create_dict(self):
+        data = {
+            "headline": self.headline,
+            "description": self.description,
+            "creation_time": self.creation_time.isoformat(),
+            "task_id": self.task_id
+        }
+        return data
 
-# connect to primary database
-primary_client = MongoClient(f'mongodb://{primary}:27017/')
-primary_db = primary_client['myproject']
-primary_tasks = primary_db['tasks']
 
-# connect to second database
-secondary_client = MongoClient(f'mongodb://{secondary}:27017/')
-secondary_db = secondary_client['myproject']
-secondary_tasks = secondary_db['tasks']
+class DBConnection():
+    def __init__(self, client: str):
+        self.client = MongoClient(f'mongodb://{client}:27017/')
+        self.db = self.client['database']
+        self.tasks = self.db['tasks']
+
+
+primary_db = DBConnection(primary)
+secondary_db = DBConnection(secondary)
 
 
 # need to change to post at the end
@@ -38,10 +47,10 @@ async def create_task(headline: str, description: str):
     new_task = Task(headline, description)
 
     # Insert the new task into the primary database
-    result_primary = primary_tasks.insert_one(new_task.dict())
+    result_primary = primary_db.tasks.insert_one(new_task.create_dict())
 
     # Insert the new task into the secondary database
-    result_secondary = secondary_tasks.insert_one(new_task.dict())
+    result_secondary = secondary_db.tasks.insert_one(new_task.create_dict())
 
     # Check that the task was successfully added to both databases
     if result_primary.inserted_id and result_secondary.inserted_id:
@@ -53,18 +62,18 @@ async def create_task(headline: str, description: str):
 @app.get("/tasks/get_tasks")
 async def get_tasks():
     tasks = []
-    all_tasks = primary_tasks.find()
+    all_tasks = primary_db.tasks.find()
     for task in all_tasks:
         tasks.append(task)
     return {"tasks": tasks}
 
 
-@app.delete("tasks/remove_task")
+@app.delete("/tasks/remove_task")
 async def remove_task(task_id: str):
-    result_primary = primary_tasks.delete_one({"task_id": task_id})
+    result_primary = primary_db.tasks.delete_one({"task_id": task_id})
 
     # Delete the task from the secondary database
-    result_secondary = secondary_tasks.delete_one({"task_id": task_id})
+    result_secondary = secondary_db.tasks.delete_one({"task_id": task_id})
 
     # Check that the task was successfully deleted from both databases
     if result_primary.deleted_count and result_secondary.deleted_count:
