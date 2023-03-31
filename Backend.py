57250f2fb2e8009c8307a835a4bd93f58dd4f4ee
jordi <yeruyarden@gmail.com>
2023-03-31 19:37:19 +0300

    connection to 2 Databases and the task creation

--- Backend.py
+++ Backend.py
@@ -1,42 +1,63 @@
 from fastapi import FastAPI, HTTPException
 import uuid
 from datetime import datetime
+from pymongo import MongoClient
+import argparse
 
+parser = argparse.ArgumentParser(description='Start the service on two servers.')
+parser.add_argument('primary', help='IP address of the primary server')
+parser.add_argument('secondary', help='IP address of the secondary server')
+args = parser.parse_args()
+primary = args.primary
+secondary = args.secoundery
 app = FastAPI()
 
 
 class Task():
-    def __init__(self, description: str, username: str):
-        #self.headline = headline
+    def __init__(self, description: str, headline: str):
+        self.headline = headline
         self.description = description
         self.creation_time = datetime.now()
         self.task_id = str(uuid.uuid4())
 
 
-headlines_to_tasks = {}
-user_to_tasks = {}
+# connect to primary database
+primary_client = MongoClient(f'mongodb://{primary}:27017/')
+primary_db = primary_client['myproject']
+primary_tasks = primary_db['tasks']
+
+# connect to second database
+secondary_client = MongoClient(f'mongodb://{secondary}:27017/')
+secondary_db = secondary_client['myproject']
+secondary_tasks = secondary_db['tasks']
 
 
-@app.get("/tasks/create_task")
+# need to change to post at the end
+@app.post("/tasks/create_task")
 async def create_task(headline: str, description: str):
-    try:
-        task = Task(headline, description)
+    new_task = Task(headline, description)
 
-        if headline in tasks:
-            # if it does, append the new task to the existing list
-            tasks[headline].append(task)
-        else:
-            # if it doesn't, create a new array with the new task
-            tasks[headline] = [task]
-        return {"status_code": 201}
-    except Exception:
-        raise HTTPException(status_code=400, detail="try again!")
+    # Insert the new task into the primary database
+    result_primary = primary_tasks.insert_one(new_task.dict())
 
+    # Insert the new task into the secondary database
+    result_secondary = secondary_tasks.insert_one(new_task.dict())
 
-@app.get("/tasks/tasks_list")
-async def tasks_list():
-    return tasks
+    # Check that the task was successfully added to both databases
+    if result_primary.inserted_id and result_secondary.inserted_id:
+        return {"message": "Task added successfully"}
+    else:
+        raise HTTPException(status_code=500, detail="Failed to add task")
 
 
-#@app.delete("tasks/remove_task")
-#async def remove_task(headline: str, assigned_to: str):
+@app.get("/tasks/get_tasks")
+async def get_tasks():
+
+
+# @app.delete("tasks/remove_task")
+# async def remove_task(headline: str, assigned_to: str):
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
